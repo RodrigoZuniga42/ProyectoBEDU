69469518f4176122a015ca68188289b5ec1b894e
RodrigoZuniga42 <109237650+RodrigoZuniga42@users.noreply.github.com>
2024-05-14 18:36:53 -0600

    Hidden funtionality test

--- utils.js
+++ utils.js
@@ -14,46 +14,29 @@ registrarseBtn.addEventListener('click', () => {
     formularioInicioSesion.classList.add('oculto');
 });
 
-class Navbar extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({ mode: 'open' });
+//Registro
+const clienteBtn = document.getElementById('clienteBtn');
+const profesionalBtn = document.getElementById('profesionalBtn');
+const formularioCliente= document.getElementById('formularioCliente');
+const formularioProfesional = document.getElementById('formularioProfesional');
 
-        this.shadowRoot.innerHTML = `
-         
-        <nav class="navbar navbar-expand-lg navbar-light bg-light" >
-    <div class="container-fluid" >
-      <a class="navbar-brand" href="index.html"><img src="assets/laplace-high-resolution-logo-transparent.png" style="height:75px; width: 125px;"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          
-          
-          <li class="nav-item">
-            <a class="nav-link" href="login.html">Login</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Services
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Legal Specialist</a></li>
-              <li><a class="dropdown-item" href="#">Consultor</a></li>
-              
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  `
+// clienteBtn.addEventListener('click', () =>{
+//   formularioCliente.classList.remove('oculto');
+// });
 
-    }
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const clienteBtn = document.getElementById('clienteBtn');
+  const formularioCliente = document.getElementById('formularioCliente');
 
-customElements.define('nav-template', Navbar);
+  if (clienteBtn && formularioCliente) {
+    clienteBtn.addEventListener('click', () => {
+      console.log('Button clicked!');
+      formularioCliente.classList.remove('oculto');
+    });
+  } else {
+    console.log('Elements not found:', {
+      clienteBtn,
+      formularioCliente
+    });
+  }
+});

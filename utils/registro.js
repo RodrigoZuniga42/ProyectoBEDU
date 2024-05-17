10d376f11a67d97eafd46d2e6b4dc04b999b714a
RodrigoZuniga42 <109237650+RodrigoZuniga42@users.noreply.github.com>
2024-05-17 16:01:54 -0600

    infy advance

--- utils/registro.js
+++ utils/registro.js
@@ -1,10 +1,18 @@
-// Registro
+// Registro & Botones
+const btnCliente=document.getElementById('btnCliente');
+const btnProfesional= document.getElementById('btnProfesional');
+
 document.getElementById('btnCliente').addEventListener('click', function() {
+  
+    btnCliente.setAttribute("disabled", true);
+    btnProfesional.removeAttribute("disabled");
     document.getElementById('formCliente').style.display = 'block';
     document.getElementById('formProfesional').style.display = 'none';
   });
   
   document.getElementById('btnProfesional').addEventListener('click', function() {
+    btnProfesional.setAttribute("disabled", true);
+    btnCliente.removeAttribute("disabled");
     document.getElementById('formProfesional').style.display = 'block';
     document.getElementById('formCliente').style.display = 'none';
   });
